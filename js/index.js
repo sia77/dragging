42027a865959a2c7b9d0ef3f6a72438aed2dad95
Sia <siavash_p@hotmail.com>
2017-07-18 01:15:13 -0700

    Putting it all together

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+var handle = document.getElementById("handle_1");
+
+
+handle.addEventListener("mousedown", function(e){ 
+
+	console.log("mousedown");
+
+
+});
+
+/*document.addEventListener("mousemove", function(e){
+	console.log("X: ", e.clientX);
+	console.log("Y: ", e.clientY);
+});*/
+
+
