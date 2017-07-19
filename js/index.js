87dc6dd85eb4fd3c464a4940043546b8219e1da6
Sia <siavash_p@hotmail.com>
2017-07-18 20:39:30 -0700

    draggin buttton

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
 
 var handle = document.getElementById("handle_1");
-
+var getCurrentElePos = function(el){
+	return {top: el.getBoundingClientRect().top, left: el.getBoundingClientRect().left };
+}
+var x_offset = getCurrentElePos(handle).left;
+console.log('x_offset', x_offset);
 
 handle.addEventListener("mousedown", function(e){ 
 
-	console.log("mousedown");
-
+	document.addEventListener("mousemove", myfunc );
 
 });
 
-/*document.addEventListener("mousemove", function(e){
-	console.log("X: ", e.clientX);
-	console.log("Y: ", e.clientY);
-});*/
 
+var myfunc = function(e){
+
+	var handle = document.getElementById("handle_1");
+	console.log('x_offset: ', x_offset);
+
+	handle.style.left = (e.clientX-x_offset) +"px";
+
+	console.log('e: ', e.type);
+
+}
+
+document.addEventListener("mouseup", function(e){ 
+
+	document.removeEventListener('mousemove', myfunc);
+});
+
+window.addEventListener('resize', function(e){
+	console.log('resize');
+	x_offset = getCurrentElePos(handle).left;
+	console.log('x_offset', x_offset);
+});
 
